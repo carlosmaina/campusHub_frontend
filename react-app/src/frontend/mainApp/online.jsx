d9445c3be4f9f3ef285e0d5ae73eb291ebdbead3
carlosmaina <carlosmaina198@gmail.com>
2025-11-21 06:02:11 +0000

    Update API endpoint URL in online.jsx

--- react-app/src/frontend/mainApp/online.jsx
+++ react-app/src/frontend/mainApp/online.jsx
@@ -83,7 +83,7 @@ function Search({ toggle }) {
     setErrorMsg("");
 
     try {
-      const res = await fetch("https://campushub-pmja.onrender.com/api", {
+      const res = await fetch("https://campushub-mq9h.onrender.com/api", {
         method: "POST",
         headers: { "content-Type": "application/json" },
         body: JSON.stringify({ val: query }),
